13e51c4a20ffeb31a57d4a206cb246891a494c59
Joe Mercer <mynameisjoemercer@gmail.com>
2015-04-12 01:17:51 -0700

    add a button

--- client/client.js
+++ client/client.js
@@ -1,1 +1,15 @@
 
+Meteor.subscribe('allPublicButtons');
+
+AutoForm.hooks({
+  insertButtonForm: {
+    before: {
+      insert: function(doc) {
+        doc.owner = Meteor.userId();
+        var now = new Date();
+        doc.clientCreatedTime = now;
+        return doc;
+      },
+    }
+  }
+});
--- server/subscriptions.js
+++ server/subscriptions.js
@@ -1,21 +1,43 @@
 
+
+
 Buttons.allow({
-  insert: function() {
+  insert: function(userId, doc) {
+  	if (!userId) {
+  		return false;
+  	}
+  	if (!(userId === doc.owner)) {
+  		return false;
+  	}
+  	var now = new Date();
+  	doc.serverCreatedTime = now;
+  	doc.serverModifiedTime = now;
     return true;
   },
-  remove: function() {
+  remove: function(userId, doc) {
+  	if (!userId) {
+  		return false;
+  	}
     return true;
   },
-  update: function() {
+  update: function(userId, doc, fieldNames, modifier) {
+  	if (!userId) {
+  		return false;
+  	}
+  	if (!(userId === doc.owner)) {
+  		return false;
+  	}
+  	modifier.$set = modifier.$set || {};
+  	modifier.$set.serverModifiedTime = new Date();
     return true;
   }
 });
 
 Clicks.allow({
-  insert: function() {
+  insert: function(userId, doc) {
     return true;
   },
-  remove: function() {
+  remove: function(userId, doc) {
     return true;
   }
 });
